b6344f4240767e2bc9b199902e596382a64f5fdf
Rice-Cameron <cameron5237@gmail.com>
2025-06-23 13:39:04 -0700

    remove old caching packages

--- src/lib/QueryClientProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export function QueryClientProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <QueryClientProvider>
-      {children}
-    </QueryClientProvider>
-  );
-}
