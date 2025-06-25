b64b2eac555786a3beca1a23f1eb72ba87ff036e
Rice-Cameron <cameron5237@gmail.com>
2025-06-24 21:49:12 -0700

    add maintenance mode

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
 import type { NextConfig } from "next";
-import * as path from 'path';
+import * as path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? {
+            source: "/((?!maintenance)(?!_next)(?!static).*)",
+            destination: "/maintenance.html",
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean) as any;
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.join(__dirname, 'src'),
+      "@": path.join(__dirname, "src"),
     };
     return config;
   },
