ca107d2c7a4a7e86244ed5015fac6f0133b7a088
Rice-Cameron <cameron5237@gmail.com>
2025-06-24 21:52:48 -0700

    fix for recent commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/problems(.*)",
@@ -6,7 +7,25 @@ const isProtectedRoute = createRouteMatcher([
   "/api/problems(.*)",
 ]);
 
+const MAINTENANCE_MODE = process.env.MAINTENANCE_MODE === "1";
+
 export default clerkMiddleware(async (auth, req) => {
+  // Maintenance mode logic
+  const { pathname } = req.nextUrl;
+  if (
+    MAINTENANCE_MODE &&
+    !pathname.startsWith("/_next") &&
+    !pathname.startsWith("/static") &&
+    pathname !== "/maintenance.html" &&
+    !pathname.match(
+      /\.(?:css|js|json|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)$/
+    )
+  ) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/maintenance.html";
+    return NextResponse.rewrite(url);
+  }
+
   if (isProtectedRoute(req)) await auth.protect();
 });
 
